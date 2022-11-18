0fa41b8746998bf883a076107d522d96281d76a5
John DeVries <johnkdevries@gmail.com>
2022-11-18 12:01:11 -0800

    Fixed paths so image script would work on unix and windows.

--- _scripts/compress-images.py
+++ _scripts/compress-images.py
@@ -22,32 +22,37 @@ from PIL import ExifTags
 from PIL import Image
 
 home = str(Path.home())
-read_directory = (home + r'\Git\exip\_images\original')
-write_directory = (home + r'\Git\exip\_images\compressed')
+# UNIX
+read_directory = (home + r'/Git/exip/_images/original')
+write_directory = (home + r'/Git/exip/_images/compressed')
 
 already_compressed_files = [f for f in listdir(write_directory) if isfile(join(write_directory, f))]	
 
 for filename in os.listdir(read_directory):
-	if filename.endswith('.jpg') and (filename not in already_compressed_files):
-		
-		# Gather path info	
-		image_path = str(read_directory + '\\' + '\\' + filename) # Weird Windows file path garbage
-		print(image_path)
-		img = Image.open(image_path)
-		
-		# Get and fix size	
-		width_pixels = img.size[0]
-		height_pixels = img.size[1]
-		adjusted_width_pixels = math.ceil((int(img.size[0]) / 2))
-		adjusted_height_pixels = math.ceil((int(img.size[1]) / 2))
-		img = img.resize((adjusted_width_pixels, adjusted_height_pixels), Image.ANTIALIAS)
-	
-		# Write new compressed image
-		write_path = str(write_directory + '\\' + '\\' + filename) # Weird Windows file path garbage
-		print('writing image...')
-		print(filename)	
-		img.save(write_path, optimize=True, quality=75)
-	
-	else:
-		continue
+    if (filename.endswith('.jpg') or filename.endswith('.png')) and (filename not in already_compressed_files):
+        # Gather path info
+        if os.name == 'nt':
+            image_path = str(read_directory + '\\' + '\\' + filename) # Weird Windows file path garbage
+        else: 
+            image_path = str(read_directory + '/' + filename)
+        print(image_path)
+        img = Image.open(image_path)
+
+        # Get and fix size
+        width_pixels = img.size[0]
+        height_pixels = img.size[1]
+        adjusted_width_pixels = math.ceil((int(img.size[0]) / 2))
+        adjusted_height_pixels = math.ceil((int(img.size[1]) / 2))
+        img = img.resize((adjusted_width_pixels, adjusted_height_pixels), Image.ANTIALIAS)
+    
+        # Write new compressed image
+        if os.name == 'nt':
+            write_path = str(write_directory + '\\' + '\\' + filename) # Weird Windows file path garbage
+        else: 
+            write_path = str(write_directory + '/' + filename)
+        print('writing image...')
+        print(filename)
+        img.save(write_path, optimize=True, quality=75)
+    else:
+        continue
 
